bb52d8c238baaab9734ae1fb56f77a54a4d67678
rilways67-star <rilways67@gmail.com>
2026-05-06 07:48:36 +0100

    Update snipe.js

--- snipe.js
+++ snipe.js
@@ -1409,7 +1409,7 @@ async function showMainMenu(ctx, edit = false) {
     }
     
     const message = `
-🚀 *Welcome to Trojan Trade Bot* 🤖
+🚀 *Welcome to MevX Trade Bot* 🤖
 
 *I'm your Web3 execution engine*.
 ━━━━━━━━━━━━━━━━━━
